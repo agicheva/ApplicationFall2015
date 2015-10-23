f10519fed55fefa32e8099b5562e9de914720559
Ani Iwanowa <anchoiwanowa@gmail.com>
2015-10-23 21:12:08 +0300

    Application Fall 2015

---
+++ Points/Points.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Points
+{
+    class Points
+    {
+        static bool isReverse = false;
+        static int xCoordinate;
+        static int yCoordinate;
+
+        static void Reverse()
+        {
+            isReverse = true;
+        }
+
+        static void IncreaseX(bool isReverse)
+        {
+            if (isReverse)
+            {
+                xCoordinate--;
+            }
+            else
+            {
+                xCoordinate++;
+            }
+        }
+
+        static void DecreaseX(bool isReverse)
+        {
+            if (isReverse)
+            {
+                xCoordinate++;
+            }
+            else
+            {
+                xCoordinate--;
+            }
+        }
+
+        static void IncreaseY(bool isReverse)
+        {
+            if (isReverse)
+            {
+                yCoordinate++;
+            }
+
+            else
+            {
+                yCoordinate--;
+            }
+        }
+
+        static void DecreaseY(bool isReverse)
+        {
+            if (isReverse)
+            {
+                yCoordinate--;
+            }
+            else
+            {
+                yCoordinate++;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Starting point: ");
+            string startingPoint = Console.ReadLine();      //User enters the coordinates of the starting point in format (x, y)
+
+            string[] splittedStartingPoint = startingPoint.Split(new char[] { '(', ')', ',' }, StringSplitOptions.RemoveEmptyEntries);
+            Console.Write("Enter the directions:");
+            string directions = Console.ReadLine();
+
+            xCoordinate = int.Parse(splittedStartingPoint[0]);
+            yCoordinate = int.Parse(splittedStartingPoint[1]);
+
+            for (int i = 0; i < directions.Length; i++)
+            {
+                if (directions[i] == '~')
+                {
+                    Reverse();
+                }
+
+                else if (directions[i] == '>')
+                {
+                    IncreaseX(isReverse);
+                }
+
+                else if (directions[i] == '<')
+                {
+                    DecreaseX(isReverse);
+                }
+
+                else if (directions[i] == '^')
+                {
+                    DecreaseY(isReverse);
+                }
+
+                else if (directions[i] == 'v')
+                {
+                    IncreaseY(isReverse);
+                }
+            }
+
+            Console.WriteLine("({0},{1})", xCoordinate, yCoordinate);
+        }
+    }
+}
---
+++ WordGame/WordGame.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordGame
+{
+    class WordGame
+    {
+        static void Main(string[] args)
+        {
+            char[,] myMatrix = { 
+                { 'i', 'v', 'a', 'n'},
+                { 'e', 'v', 'n', 'h'},
+                { 'i', 'n', 'a', 'v'},
+                { 'n', 'v', 'v', 'n'},
+                { 'r', 'r', 'i', 't'}
+            };                          
+
+            string myWord = "ivan";
+
+            for (int i = 0; i < myMatrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < myMatrix.GetLength(1); j++)
+                {
+                    checkForWordRight(i, j , myMatrix, myWord);
+                    checkForWordLeft(i, j, myMatrix, myWord);
+                    checkForWordUp(i, j, myMatrix, myWord);
+                    checkForWordDown(i, j, myMatrix, myWord);
+                    checkForWordDiagonalUp(i, j, myMatrix, myWord);
+                    checkForWordDiagonalDown(i, j, myMatrix, myWord);
+                }
+               
+            }
+            Console.WriteLine(counter);
+        }
+
+        static int counter = 0;
+
+        static void checkForWordRight(int row, int col, char[,] matrix, string word)
+        {
+            Boolean isFound = true;
+            for (int i = 0; i < word.Length; i++)
+            {
+                try
+                {
+                    if (word[i] != matrix[row, col + i])
+                    {
+                        isFound = false;
+                        break;
+                    }
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    isFound = false;
+                }
+            }
+
+            if (isFound)
+            {
+                counter++;
+            }
+        }
+
+        static void checkForWordLeft(int row, int col, char[,] matrix, string word)
+        {
+            Boolean isFound = true;
+            for (int i = 0; i < word.Length; i++)
+            {
+                try
+                {
+                    if (word[i] != matrix[row, col - i])
+                    {
+                        isFound = false;
+                        break;
+                    }
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    isFound = false;
+                }
+            }
+
+            if (isFound)
+            {
+                counter++;
+            }
+        }
+
+        static void checkForWordUp(int row, int col, char[,] matrix, string word)
+        {
+            Boolean isFound = true;
+            for (int i = 0; i < word.Length; i++)
+            {
+                try
+                {
+                    if (word[i] != matrix[row + i, col])
+                    {
+                        isFound = false;
+                        break;
+                    }
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    isFound = false;
+                }
+            }
+
+            if (isFound)
+            {
+                counter++;
+            }
+        }
+
+        static void checkForWordDown(int row, int col, char[,] matrix, string word)
+        {
+            Boolean isFound = true;
+            for (int i = 0; i < word.Length; i++)
+            {
+                try
+                {
+                    if ( word[i] != matrix[row - i, col])
+                    {
+                        isFound = false;
+                        break;
+                    }
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    isFound = false;
+                }
+            }
+
+            if (isFound)
+            {
+                counter++;
+            }
+        }
+
+        static void checkForWordDiagonalUp(int row, int col, char[,] matrix, string word)
+        {
+            Boolean isFound = true;
+            for (int i = 0; i < word.Length; i++)
+            {
+                try
+                {
+                    if ( word[i] != matrix[row + i, col + i])
+                    {
+                        isFound = false;
+                        break;
+                    }
+                }
+                catch (Exception)
+                {
+                    isFound = false;
+
+                }
+            }
+
+            if (isFound)
+            {
+                counter++;
+            }
+        }
+
+        static void checkForWordDiagonalDown(int row, int col, char[,] matrix, string word)
+        {
+            Boolean isFound = true;
+            for (int i = 0; i < word.Length; i++)
+            {
+                try
+                {
+                    if ( word[i] != matrix[row - i, col - i])
+                    {
+                        isFound = false;
+                        break;
+                    }
+                }
+                catch (IndexOutOfRangeException e)
+                {
+                    isFound = false;
+                }                
+            }
+
+            if (isFound)
+            {
+                counter++;
+            }
+        }
+
+    }
+}
